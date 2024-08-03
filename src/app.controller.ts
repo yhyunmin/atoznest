9e2df84a6b6225962b27ef1728c920ee5e3b9312
smurf <smurf@kakao.com>
2024-08-04 03:16:29 +0900

    added getPost,type Post

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/*
+ * author:string;
+ * title:string;
+ * content:string;
+ * likeCount:number;
+ * commentCount:number;
+ */
+
+type Post = {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+};
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +23,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get()
+  getPost(): Post {
+    return {
+      author: 'newJeans_official',
+      title: '제목입니다.',
+      content: '내용입니다.',
+      likeCount: 0,
+      commentCount: 0,
+    };
   }
 }
