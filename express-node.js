02342b5cf0554a42ff8295bd6163a21a5d679de2
smurf <smurf@kakao.com>
2024-08-04 02:46:54 +0900

    test express

---
+++ express-node.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.send('<h1>Home Page</h1>');
+});
+app.get('/post',(req,res)=>{
+  res.send('<h1>Post Page</h1>');
+});
+app.get('/user',(req,res)=>{
+  res.send('<h1>User Page</h1>');
+});
+app.use((req,res)=>{
+  res.status(404).send('<h1>Page Not Found</h1>');
+})
+app.listen(3000,()=>{
+  console.log('Listening on port 3000');
+});
