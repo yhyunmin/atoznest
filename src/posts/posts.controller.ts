7562c84a791bdd17e28c93d3dec0ed163348b43c
smurf <smurf@kakao.com>
2024-08-06 11:31:30 +0900

    modify posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -8,25 +8,47 @@ import { PostsService } from './posts.service';
  * commentCount:number;
  */
 
-type Post = {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
+type PostModel = {
+  id: number; author: string; title: string; content: string; likeCount: number; commentCount: number;
 };
+let posts: PostModel[] = [ {
+  id:1,
+  author:'DummyAuthor',
+  title:'DummyTitle',
+  content:'DummyContent',
+  likeCount:0,
+  commentCount:0,
+}, {
+  id:2,
+  author:'DummyAuthor2',
+  title:'DummyTitle2',
+  content:'DummyContent2',
+  likeCount:1,
+  commentCount:1,
+},
+  {
+    id:3,
+    author:'DummyAuthor3',
+    title:'DummyTitle3',
+    content:'DummyContent3',
+    likeCount:2,
+    commentCount:2,
+  },
+];
+
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) {}
-
-  @Get()
-  getPost(): Post {
-    return {
-      author: 'newJeans_official',
-      title: '제목입니다.',
-      content: '내용입니다.',
-      likeCount: 0,
-      commentCount: 0,
-    };
+  constructor (private readonly postsService: PostsService) {
   }
+  
+  // Get /posts
+  @Get()
+  getPosts(){
+    return posts;
+  }
+  // Get /posts/:id
+  
+  // POST /posts
+  // PUT /posts/:id
+  // DELETE /popts/:id
 }
